2b8ca7170fdc12581f3897ae4598b8ecae608743
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-21 01:08:16 +0100

    fix: rename file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 
-import data from './services/peopleService';
+import data from './data';
 
 import './App.css';
 
