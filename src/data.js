569d27ac42f5e65efc63c9239a4423438b6cfa24
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-21 01:10:32 +0100

    fix: modify

--- src/data.js
+++ src/data.js
@@ -29,9 +29,9 @@ const people = [
   {
     id: 4,
     image:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    name: "susan andersen",
-    title: "the boss",
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: 'susan andersen',
+    title: 'the boss',
     quote:
       "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
   },
