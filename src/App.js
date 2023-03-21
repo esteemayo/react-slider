15a5b037b994a2f0405f7a4775d8d162307cfc76
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-21 01:04:17 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 
-import data from "./services/peopleService";
+import data from './services/peopleService';
 
 import "./App.css";
 
