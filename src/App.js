58aae0eb2b2c4cb118b15ec2732f26fdd3ea930c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-21 01:05:38 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -55,9 +55,9 @@ function App() {
             <article key={id} className={position}>
               <img src={image} alt={name} className='person-img' />
               <h4>{name}</h4>
-              <p className="title">{title}</p>
-              <p className="text">{quote}</p>
-              <FaQuoteRight className="icon" />
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
             </article>
           );
         })}
